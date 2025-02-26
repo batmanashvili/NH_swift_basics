4c29cc36eafba2d7c4df36f085957ede92401e00
Batmanashvili <beka.barmanashvili.ext@immfly.com>
2025-02-26 10:08:38 +0400

    feat: initialize week4 and add app icon

---
+++ week4/Week4/Week4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week4
+//
+//  Created by Beka Batmanashvili on 26.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ week4/Week4/Week4/Week4App.swift
@@ -0,0 +1,17 @@
+//
+//  Week4App.swift
+//  Week4
+//
+//  Created by Beka Batmanashvili on 26.02.25.
+//
+
+import SwiftUI
+
+@main
+struct Week4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
